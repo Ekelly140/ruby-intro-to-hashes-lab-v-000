6b204ceef0a96d8482c7ac69634c7411109e5c18
ERIC KELLY <eakelly220@gmail.com>
2018-04-18 20:59:15 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,10 +44,10 @@ end
 def monopoly_with_fourth_tier
 	monopoly = {railroads: {
 	  pieces: 4,
-	  names: {reading_railroad:{},
-	  pennsylvania_railroad:{},
-	  b_and_o_railroad:{},
-	  shortline:{}
+	  names: {reading_railroad:{mortage_value: "$100"},
+	  pennsylvania_railroad:{mortage_value: "$200"},
+	  b_and_o_railroad:{mortage_value: "$400"},
+	  shortline:{mortage_value: "$800"}
 	  },
 	  rent_in_dollars: {
 	    one_piece_owned: 25,
